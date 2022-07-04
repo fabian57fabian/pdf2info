44930a0758ba7bed31d286438b448dabf51503c8
fabian57fabian <fabian_57@yahoo.it>
2022-07-04 18:12:56 +0200

    Added logging with extra command for console logging

--- pdf2info.py
+++ pdf2info.py
@@ -1,5 +1,6 @@
 import tabula
-
+import logging
+logging.getLogger('tabula').setLevel(logging.CRITICAL)
 import os
 
 
@@ -11,20 +12,20 @@ def extract_from_pdf(src_doc: str, out_folder: str) -> (bool, int):
     :return: tuple(bool, int) Extraction result and number of tables extracted.
     """
     if not os.path.exists(src_doc):
-        print("Input pdf does not exist: " + str(src_doc))
+        logging.error("Input pdf does not exist: " + str(src_doc))
         return False, 0
     if not os.path.exists(out_folder):
         os.mkdir(out_folder)
     file_name = os.path.basename(src_doc)
-    print("processing " + file_name)
+    logging.info("processing " + file_name)
 
     # Tabula:
     try:
         tables = tabula.read_pdf(src_doc, pages="all")
     except Exception as e:
-        print("Skipping because error opening")
+        logging.error("Skipping because error opening: "+str(e))
         return False, 0
-    print("Found {} tables with tabula".format(len(tables)))
+    logging.debug("Found {} tables with tabula".format(len(tables)))
     for i, table in enumerate(tables):
         out_path = os.path.join(out_folder, "{}_{}.csv".format(file_name[:-4], i))
         table.to_csv(out_path)
@@ -39,7 +40,7 @@ def extract_from_dir(src_dir, out_folder) -> (int, int, int):
     :return: Total tables extracted, # of pdfs with errors on read, total pdf analyzed
     """
     if not os.path.exists(src_dir):
-        print("Input dir does not exist: " + str(src_dir))
+        logging.error("Input dir does not exist: " + str(src_dir))
         return False
     total_tables, errored_pdfs, tot_pdf = 0, 0, 0
     for file in os.listdir(src_dir):
--- process_folder.py
+++ process_folder.py
@@ -1,15 +1,35 @@
 from pdf2info import extract_from_dir
+import logging
 import argparse
+
+
+def init_logging(lvl, fn):
+    if fn is None:
+        logging.basicConfig(
+            level=lvl,
+            format='%(process)d-%(levelname)s-%(message)s',
+            datefmt='%H:%M:%S'
+        )
+    else:
+        logging.basicConfig(
+            filename='result.log',
+            level=lvl,
+            format='%(process)d-%(levelname)s-%(message)s',
+            datefmt='%H:%M:%S'
+        )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--dir', type=str, help='Directory with PDFs')
     parser.add_argument('--out', type=str, help='Out directory')
+    parser.add_argument('--log-console', action='store_true', default=False, help='Out directory')
     args = parser.parse_args()
+
+    init_logging(logging.INFO, None if args.log_console else "result.log")
 
     src_dir = args.dir
     out_folder = args.out
     total_tables, errored_pdfs, tot_pdf = extract_from_dir(src_dir, out_folder)
-    print("pdfs with errors: {}/{} ".format(errored_pdfs, tot_pdf))
-    print("tables extracted: {}".format(total_tables))
+    logging.info("pdfs with errors: {}/{} ".format(errored_pdfs, tot_pdf))
+    logging.info("tables extracted: {}".format(total_tables))
