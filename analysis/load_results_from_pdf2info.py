c5162b956e7d1acb30b579fb1e04b89f7f73390b
fabian57fabian <fabian_57@yahoo.it>
2022-11-19 20:30:58 +0100

    added pdf2info result. Ignored chunks in analysis

--- analysis/load_results_from_pdf2info.py
+++ analysis/load_results_from_pdf2info.py
@@ -7,7 +7,7 @@ from core.results_loader import load_results_from_csv
 if __name__ == '__main__':
     folder_csv = "csv_extracted"
     out_result = "pdf2info_results.csv"
-    tables_out, total_tables = load_results_from_csv(folder_csv)
+    tables_out, total_tables = load_results_from_csv(folder_csv, split_char='_')
     print("Total tables: {}".format(total_tables))
     with open(out_result, 'w') as file:
         file.write("pdf,figures")
--- core/results_loader.py
+++ core/results_loader.py
@@ -1,13 +1,13 @@
 import os
 
 
-def load_results_from_csv(csv_path) -> (dict, int):
+def load_results_from_csv(csv_path, split_char='-') -> (dict, int):
     tables_out = {}
     total_tables = 0
     for file in os.listdir(csv_path):
         if file.endswith(".csv"):
             # file format is [pdf_name]-Table[table_number].csv
-            splat = file.split('-')
+            splat = file.split(split_char)
             if len(splat) >= 2:
                 pdf_name = splat[0] + ".pdf"
                 if pdf_name not in tables_out:
