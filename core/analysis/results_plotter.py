151a48af7d03b663bdb67928337523546e5645ab
fabian57fabian <fabiangreavu@gmail.com>
2022-11-21 14:53:25 +0100

    Added script to plot heatmpas as result

---
+++ core/analysis/results_plotter.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_results(res):
+    plt.figure(figsize=(9,9))
+    a = np.array(res)
+    ns = np.ceil(np.sqrt(len(res))).astype(int)
+    s = np.zeros(ns ** 2)
+    s[:a.size] = a
+    y = s.reshape(ns, ns)
+
+    plt.imshow(y, cmap='Reds', interpolation='nearest')
+    plt.show()
+
+
+if __name__ == '__main__':
+    test_data = [1,3,4,5,1,24,25,245,45,2,11,31,214,3,24,124,1,1,2]
+    plot_results(test_data)
