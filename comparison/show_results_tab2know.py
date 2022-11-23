ccf8c1fb5b2a6b8527d97d12122b3a1d9b6d1932
fabian57fabian <fabiangreavu@gmail.com>
2022-11-23 14:23:39 +0100

    removed load_results. Updated test files. Added show results for tab2know

---
+++ comparison/show_results_tab2know.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(0, "../")
+from core.analysis.results_plotter import plot_results
+from core.analysis.check_results_over_groundtruth import load_groundtruth, check_results
+
+
+
+
+if __name__ == '__main__':
+    csv_folder="csv_extracted"
+    gth = load_groundtruth("DATASET_PDFS_50/GROUNDTRUTH_DATASET.csv")
+    res, res_percentage = check_results(csv_folder, gth)
+    # res into list
+    results = res_percentage
+    plot_results(results, color_min=0, color_max=100)
