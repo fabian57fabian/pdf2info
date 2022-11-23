f054e2ce29946fbfac838bad905ae670e68c9071
fabian57fabian <fabiangreavu@gmail.com>
2022-11-23 14:48:41 +0100

    Solved checking results bug. Added right results for tested tab2know and pdf2info

--- analysis/show_results_pdf2info.py
+++ analysis/show_results_pdf2info.py
@@ -9,7 +9,7 @@ from core.analysis.check_results_over_groundtruth import load_groundtruth, check
 if __name__ == '__main__':
     csv_folder="csv_extracted"
     gth = load_groundtruth("DATASET_PDFS_50/GROUNDTRUTH_DATASET.csv")
-    res, res_percentage = check_results(csv_folder, gth)
+    res, res_percentage = check_results(csv_folder, gth, split_char='_')
     # res into list
     results = res_percentage
     plot_results(results, color_min=0, color_max=100)
--- core/analysis/check_results_over_groundtruth.py
+++ core/analysis/check_results_over_groundtruth.py
@@ -18,26 +18,31 @@ def load_groundtruth(path:str):
     return gth
 
 
-def check_results(csv_folder:str, groundtruth:dict) ->(list, list):
+def check_results(csv_folder:str, groundtruth:dict, split_char='-') ->(list, list):
     res = []
     res_percentage = []
+    results = {}
     i = 1
+    for k,v in groundtruth.items():
+        results[k] = []
     for file_tablex in os.listdir(csv_folder):
-        logging.info("{}: {}".format(i, file_tablex))
-        i+=1
         if file_tablex.endswith(".csv"):
-            f_orig = file_tablex.split('_')[0]+".pdf"
-            filename = os.path.join(csv_folder, file_tablex)
-            found = 0
-            gth_tables = 0
+            f_orig = file_tablex.split(split_char)[0]+".pdf"
+            if f_orig not in results: results[f_orig] = []
+            results[f_orig].append(file_tablex)
+    for f_orig, tables_names in results.items():
+        logging.info("{}: {}".format(i, f_orig))
+        i += 1
+        found = 0
+        gth_tables = len(groundtruth[f_orig])
+        for table_filename in tables_names:
             try:
                 if f_orig not in groundtruth:
                     logging.warning("File {} not in groundtruth table!".format(f_orig))
                 else:
                     keys:list = [k for k in groundtruth[f_orig]]
-                    gth_tables = len(keys)
                     if gth_tables> 0:
-                        with open(filename, 'r') as file:
+                        with open(os.path.join(csv_folder, table_filename), 'r') as file:
                             for l in file.readlines():
                                 for k in keys:
                                     if k in l:
@@ -46,8 +51,11 @@ def check_results(csv_folder:str, groundtruth:dict) ->(list, list):
                                         break
             except Exception as e:
                 logging.warning("Unable to complete check on file {}: {}".format(file_tablex, str(e)))
-            res.append(found)
-            res_percentage.append(100.0 if gth_tables== 0 else float(found)/gth_tables*100)
+        res.append(found)
+        if gth_tables== 0:
+            res_percentage.append(100.0)
+        else:
+            res_percentage.append(float(found)/gth_tables*100)
     return res, res_percentage
 
 
@@ -57,4 +65,4 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
     csv_folder="../../analysis/csv_extracted"
     gth = load_groundtruth("../../analysis/DATASET_PDFS_50/GROUNDTRUTH_DATASET.csv")
-    check_results(csv_folder, gth)
+    check_results(csv_folder, gth, split_char='_')
