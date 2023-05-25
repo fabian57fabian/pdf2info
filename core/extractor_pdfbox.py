8046b2bb7f938d640804367edd2d9b370ae986c2
fabian57fabian <fabian_57@yahoo.it>
2023-05-25 14:16:12 +0200

    Added back and front table detection from 'Tab.' or 'Table'. up to 75%

--- core/extractor_pdfbox.py
+++ core/extractor_pdfbox.py
@@ -15,6 +15,11 @@ def read_and_delete_file(path) -> Optional[List[str]]:
         logging.error("Got error while reading: " + str(e))
         return None
 
+def find_numbers_in_line(line):
+    line_words = line.split(' ')
+    digits_elements = [l for l in line_words if l.replace('.','',1).replace('%','').replace('±','').isdigit()]
+    return line_words, digits_elements
+
 def load_tables_manually(path):
     global p
     tmp_fn = tempfile.NamedTemporaryFile().name + ".txt"
@@ -37,50 +42,56 @@ def load_tables_manually(path):
 
             # iterate from i backwards max 50 lies
             last_added = True
-            for k in range(1, min(i, 50)):
-                line = text[i-k]
-                line_words = line.split(' ')
-                digits_elements = [l for l in line_words if l.replace('.','',1).isdigit()]
-                added = False
-                # we are inside table if at least 1/3 of words are numbers
-                # we also allow one line skip and first line above table
-                words, digits = len(line_words), len(digits_elements)
-                line_has_digits = False
-                if digits > 1:
-                    if words == 2 and digits >= 1:
-                        line_has_digits = True
-                    if words == 3 and digits >= 1:
-                        line_has_digits = True
-                    if words == 4 and digits >= 1:
-                        line_has_digits = True
-                    else:
-                        line_has_digits = digits >= words // 3 or digits >= 2
-                if line_has_digits:
-                    # line with digits
-                    current_table.insert(0, line)
-                    added = True
+            for table_is_before in [True, False]:
+                for k in range(1, min(i, 50)):
+                    next_index = i-k if table_is_before else i+k
+                    if next_index < 0 or next_index >= len(text):
+                        break
+                    line = text[next_index]
+                    # a new table arrived
+                    if line.startswith("table") or line.startswith('tab.'):
+                        break
+                    line_words, digits_elements = find_numbers_in_line(line)
+                    added = False
+                    # we are inside table if at least 1/3 of words are numbers
+                    # we also allow one line skip and first line above table
+                    words, digits = len(line_words), len(digits_elements)
+                    line_has_digits = False
+                    if digits > 1:
+                        if words == 2 and digits >= 1:
+                            line_has_digits = True
+                        if words == 3 and digits >= 1:
+                            line_has_digits = True
+                        if words == 4 and digits >= 1:
+                            line_has_digits = True
+                        else:
+                            line_has_digits = digits >= words // 3 or digits >= 2
+                    if line_has_digits:
+                        # line with digits
+                        current_table.insert(0, line)
+                        added = True
 
-                if not added and k == 1:
-                    # last line before table
-                    current_table.insert(0, line)
-                    added = True
+                    if not added and k == 1:
+                        # last line before table
+                        current_table.insert(0, line)
+                        added = True
 
-                if not added and not last_added:
-                    break
+                    if not added and not last_added:
+                        break
 
-                if not added:
-                    current_table.insert(0, line)
-                    last_added = False
-            if len(current_table) > 1:
-                current_table = [l.replace(' ', ',') for l in current_table]
-                max_cols = max([line.count(',') for line in current_table])
-                for line in current_table:
-                    line_commas = line.count(',')
-                    if line_commas < max_cols:
-                        for _ in range(max_cols-line_commas):
-                            line += ','
-                tab = pd.DataFrame(current_table)
-                tables.append(tab)
+                    if not added:
+                        current_table.insert(0, line)
+                        last_added = False
+                if len(current_table) > 1:
+                    current_table = [l.replace(' ', ',') for l in current_table]
+                    max_cols = max([line.count(',') for line in current_table])
+                    for line in current_table:
+                        line_commas = line.count(',')
+                        if line_commas < max_cols:
+                            for _ in range(max_cols-line_commas):
+                                line += ','
+                    tab = pd.DataFrame(current_table)
+                    tables.append(tab)
     return tables
 
 
@@ -90,7 +101,8 @@ def load_tables_manually(path):
 #manual 'Table X' + float					-> 70.99 %
 #manual 'Table X' + 'Tab. X' + float				-> 72.67 %
 #manual 'Table X' + 'Tab. X' + least 3 numbs + float		-> 73.00 %
-#anual 'Table X' + 'Tab. X' + least 2 numbs + 2,3 elements + float		-> 73.17 %
+#manual 'Table X' + 'Tab. X' + least 2 numbs + 2,3 elements + float		-> 73.17 %
+#manual 'Table X' + 'Tab. X' + backwards + least 2 numbs + 2,3 elements + float		-> 75.09 %
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
