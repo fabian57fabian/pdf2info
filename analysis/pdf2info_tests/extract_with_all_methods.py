4a55c1974bc624a131d52aedb55a1563fd8a7647
fabian57fabian <fabian_57@yahoo.it>
2023-06-07 16:02:11 +0200

    Changed from pdf2info to 'method'

--- analysis/pdf2info_tests/extract_with_all_methods.py
+++ analysis/pdf2info_tests/extract_with_all_methods.py
@@ -5,8 +5,8 @@ import logging
 import argparse
 sys.path.insert(0, "../../")
 from core.pdf2info import extraction_methods_names
-from analysis.pdf2info_tests.execute_pdf2info_on_DATASET import execute_extraction_with_method
-from analysis.pdf2info_tests.show_results_pdf2info import create_plots_results
+from analysis.pdf2info_tests.execute_method_on_DATASET import execute_extraction_with_method
+from analysis.pdf2info_tests.show_results_of_method import create_plots_results
 
 if __name__ == '__main__':
     logging.basicConfig(
