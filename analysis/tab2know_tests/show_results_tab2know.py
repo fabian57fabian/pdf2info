406c8125acd640ad4514ccb3a31e58979891224a
fabian57fabian <fabian_57@yahoo.it>
2023-06-07 17:55:32 +0200

    Updated tab2know plot

--- analysis/tab2know_tests/show_results_tab2know.py
+++ analysis/tab2know_tests/show_results_tab2know.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     csv_folder="out_tab2know/csv"
     gth = load_groundtruth("../DATASET/GROUNDTRUTH_DATASET.csv")
     TP, TN, FP, FN, files_percentage_ok = check_results(csv_folder, gth, split_char='-')
-    plot_results(TP, TN, FP, FN, files_percentage_ok, color_min=0, color_max=100, save_to="../results/tab2know_on_DATASET.png")
+    plot_results(TP, TN, FP, FN, files_percentage_ok, title="tab2know: ", color_min=0, color_max=100, save_to="../results/tab2know_on_DATASET.png")
